4f13ce5797d04a39f57f6204a5b4629fcbd2054d
Alex Clark <aclark@aclark.net>
2017-10-30 07:42:38 -0400

    Update

--- garage_door_status.py
+++ garage_door_status.py
@@ -1,3 +1,5 @@
+# ------------------------------------------------------------------------------
+# Pseudocode
 # Is garage door open?
 #     #If garage door closed, go to END, otherwise next
 #     #time_opened = now
@@ -12,49 +14,51 @@
 #         if garage door closed, exit loop 1, else loop
 # END
 
-# import time
+# ------------------------------------------------------------------------------
+# Multi-door support via http://forums.indigodomo.com/viewtopic.php?f=77&t=13579#p95041
 # 
-# class GarageDoor():
-#     def __init__(self):
-#         self.position = 'CLOSED'
+# # a list of your garage door I/O Linc IDs from Indigo
+# garage_door_ids = [1053088701, 324932738] 
 # 
-# garage_door = GarageDoor()
+# # variable that holds the value of the doors
+# door_is_open = "closed"
 # 
-# while garage_door.position == 'CLOSED':
-#     print("Door is closed")
-#     time.sleep(10)
-#     garage_door.position = 'OPEN'
-#     print("Door is open")
-#     time_opened = time.time()
-#     print("Time is %s" % time_opened)
-#     while garage_door.position == 'OPEN':
-#         time.sleep(10)
-#         garage_door.position == 'CLOSED'
-#         print("Door is closed")
-#         time_closed = time.time()
-#         print("Time is %s" % time_closed)
+# # repeat for each ID in your list
+# for id in garage_door_ids:
+#     try:
+#         # if the input is not true, it's open
+#         if not indigo.devices[id].binaryInputs[0]:
+#             # set the variable
+#             door_is_open = "open"
+#             
+#             # stop the loop since we found an open door
+#             break
+#     except:
+#         pass # device might not be availble for some reason
+# 
+# # Set the variable value - insert the Indigo ID of your variable
+# indigo.variable.updateValue(1229620185, door_is_open)
 
+# ------------------------------------------------------------------------------
 
-# Via http://forums.indigodomo.com/viewtopic.php?f=77&t=13579#p95041
+import time
 
-# a list of your garage door I/O Linc IDs from Indigo
-garage_door_ids = [1053088701, 324932738] 
+class GarageDoor():
+    def __init__(self):
+        self.position = 'CLOSED'
 
-# variable that holds the value of the doors
-door_is_open = "closed"
+garage_door = GarageDoor()
 
-# repeat for each ID in your list
-for id in garage_door_ids:
-    try:
-        # if the input is not true, it's open
-        if not indigo.devices[id].binaryInputs[0]:
-            # set the variable
-            door_is_open = "open"
-            
-            # stop the loop since we found an open door
-            break
-    except:
-        pass # device might not be availble for some reason
-
-# Set the variable value - insert the Indigo ID of your variable
-indigo.variable.updateValue(1229620185, door_is_open)
+while garage_door.position == 'CLOSED':
+    print("Door is closed")
+    time.sleep(10)
+    garage_door.position = 'OPEN'
+    print("Door is open")
+    time_opened = time.time()
+    print("Time is %s" % time_opened)
+    while garage_door.position == 'OPEN':
+        time.sleep(10)
+        garage_door.position == 'CLOSED'
+        print("Door is closed")
+        time_closed = time.time()
+        print("Time is %s" % time_closed)
