7e7b17e1ed864e592f0426013c52fd2cc5693c8f
Alex Clark <aclark@aclark.net>
2018-01-09 07:35:37 -0500

    Update

--- garage_door_status.py
+++ garage_door_status.py
@@ -7,16 +7,16 @@ SUBJECT = "Garage Door Status Update"
 elapsed_time = 0
 
 indigo.server.sendEmailTo(
-    EMAIL, subject=SUBJECT, body='Waiting for door')  # Send mail
+    EMAIL, subject=SUBJECT, body='Waiting for garage door')  # Send mail
 
 while True:
-    door = indigo.devices[252434934]  # Large garage door for cars
-    state = door.states[u'binaryInput1.ui']  # Open or closed
+    garage_door = indigo.devices[252434934]  # Large garage door for cars
+    state = garage_door.states[u'binaryInput1.ui']  # Open or closed
     start_time = time.time()  # https://stackoverflow.com/a/3620972
     message = ("%s is %s (%s)" %
-               (door.name, state,
+               (garage_door.name, state,
                 elapsed_time))  # "Door is open", or "Door is closed"
-    if state != 'closed':  # If the door is not closed
+    if state != 'closed':  # If the garage door is not closed
         elapsed_time = time.time() - start_time
         indigo.server.sendEmailTo(
             EMAIL, subject=SUBJECT, body=message)  # Send mail
